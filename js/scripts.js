a8feb5b084f3d21d9adf1e99926e721764586bd3
Anna Bródka <annabrodka@interia.pl>
2017-06-06 21:50:47 +0200

    Przesyłam zadanie 8.3

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+// scripts.js
+
+var name = prompt('What\'s your name?');
+alert('Hello ' + name + '!');
+console.log('Hello ' + name + '!');
--- scripts.js
+++
@@ -1,5 +0,0 @@
-//script.js
-
-var name = prompt('What is your name?');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
