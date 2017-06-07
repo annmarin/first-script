d31a3c69c76d02f482420642a6f9ce7e8f487c89
Anna Bródka <annabrodka@interia.pl>
2017-06-07 15:22:05 +0200

    Przesyłam zadanie 8.4

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 // scripts.js
 
 var name = prompt('What\'s your name?');
-alert('Hello ' + name + '!');
+// alert('Hello ' + name + '!');
 console.log('Hello ' + name + '!');
+
+// checking the variable
+var variable = 'How are you?';
+console.log(variable);
+
+/* the way of commenting
+ is the same like for css */
